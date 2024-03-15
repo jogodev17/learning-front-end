211503b236c4326797e482cfe7d2bf9dd7d3168f
Joshua Gonzales <codingjogo@gmail.com>
2024-03-16 07:34:39 +0800

    reset
    learn new

--- prisma-postgresql/1-code-ryan-prisma-postgres-tutorial/app/api/add-post/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-
-export async function POST(request: NextResponse) {  
-    const res = await request.json();
-    console.log
-    return NextResponse.json({data: res})    
-}
