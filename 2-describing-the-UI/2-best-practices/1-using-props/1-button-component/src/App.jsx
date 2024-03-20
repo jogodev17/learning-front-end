4a8e4ba4699a26173a26a4d53bed33c0bd3cf3ce
Joshua Gonzales <jogodev17@gmail.com>
2024-03-20 14:23:51 +0800

    I centered the content in main

--- 2-describing-the-UI/2-best-practices/1-using-props/1-button-component/src/App.jsx
+++ 2-describing-the-UI/2-best-practices/1-using-props/1-button-component/src/App.jsx
@@ -6,10 +6,12 @@ function App() {
 
   return (
     <>
-      <main>
+      <main className='h-screen flex flex-col justify-center items-center'>
+        <div>
         <h1>Hello World!</h1>
         <h2>You clicked {count} times</h2>
         <MyButton className='py-2 px-6 bg-emerald-300 text-white rounded-md shadow-md' onClick={() => setCount(count + 1)} label='Increment'/>
+        </div>
       </main>
     </>
   )
